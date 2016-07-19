c6aa9682b4e790202c3b3e8a3099808e0a4fb592
Omar Makeen <omar.makeen@vodafone.com>
2016-07-19 14:54:27 +0200

    add peopleService

--- app/pages/page1/page1.ts
+++ app/pages/page1/page1.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { PeopleService } from '../../providers/people-service/people-service'
 
 @Component({
-  templateUrl: 'build/pages/page1/page1.html'
+  templateUrl: 'build/pages/page1/page1.html',
+  providers: [PeopleService]
 })
 export class Page1 {
 
-  constructor(private navController: NavController) {
+  public people:any;
+
+  constructor(private navController:NavController, public peopleService:PeopleService) {
+
+    this.loadPeople();
+
+  }
+
+  loadPeople() {
+    this.peopleService.load()
+      .then(data => {
+        this.people = data;
+      });
   }
 }
